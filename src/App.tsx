59bf0ec60d1de424450b921db81e93b7a2b9de4a
fransengstrom <frans.engstrom@gmail.com>
2021-08-18 16:51:40 +0200

    child actors communication added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {pokemonMachine} from './state/state';
+import {useMachine} from '@xstate/react';
+import {Pokemon} from "./components/pokemon/pokemon";
+
+
+const pokemonList = ['charizard', 'ditto']
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [current, send] = useMachine(pokemonMachine);
+
+    // @ts-ignore
+    let {pokemon} = current.context;
+
+    console.log("state", current.context);
+
+    return (
+        <div className="App">
+            <select
+                onChange={(e) => {
+                    send("SELECT", {name: e.target.value});
+                }}
+            >
+                {pokemonList.map((pokemon) => {
+                    return <option key={pokemon}>{pokemon}</option>;
+                })}
+            </select>
+
+            { pokemon && <Pokemon actor={pokemon} />}
+
+
+        </div>
+    );
 }
 
 export default App;
